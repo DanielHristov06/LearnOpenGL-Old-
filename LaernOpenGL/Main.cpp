4bd847318854769266635bd8ec6d6dc8e124bc59
TheEliteD <dhristov06@gmail.com>
2024-06-03 23:08:06 +0300

    window

--- LaernOpenGL/Main.cpp
+++ LaernOpenGL/Main.cpp
@@ -3,5 +3,35 @@
 #include <GLFW/glfw3.h>
 
 int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1600, 900, "Ne haresvam Ivan", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create a window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 1600, 900);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
