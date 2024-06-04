cdb4f8a1b26500f45ba9b8ec50b43458583662b8
TheEliteD <dhristov06@gmail.com>
2024-06-04 18:34:43 +0300

    triangle (tf is all that)

--- LaernOpenGL/Main.cpp
+++ LaernOpenGL/Main.cpp
@@ -2,6 +2,22 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main() {
 	//Initialize GLFW
 	glfwInit();
@@ -13,8 +29,14 @@ int main() {
 	//Tell GLFW we are using the core profile (we only have the modern functions)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLfloat vertices[] = {
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+	};
+
 	//Create a window object
-	GLFWwindow* window = glfwCreateWindow(1600, 900, "Ne haresvam Ivan", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1600, 900, "Wow, a window!", NULL, NULL);
 
 	//Error check if the window fails to create
 	if (window == NULL) {
@@ -23,31 +45,67 @@ int main() {
 		return -1;
 	}
 
-	//Introduce the window into the current context
-	glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window); //Introduce the window into the current context
 
-	//Load GLAD
-	gladLoadGL();
+	gladLoadGL(); //Load GLAD
 
-	//Specify the viewport of OpenGL in the window
-	glViewport(0, 0, 1600, 900);
+	glViewport(0, 0, 1600, 900); //Specify the viewport of OpenGL in the window
 
-	//Specify the color of the background
-	glClearColor(51.0f / 255.0f, 153.0f / 255.0f, 1.0f, 1.0f);
-	//Clean the back buffer and assign the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	//Swap the back buffer with the front buffer
-	glfwSwapBuffers(window);
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glClearColor(51.0f / 255.0f, 153.0f / 255.0f, 1.0f, 1.0f); //Specify the color of the background
+
+	glClear(GL_COLOR_BUFFER_BIT); //Clean the back buffer and assign the new color to it
+
+	glfwSwapBuffers(window);//Swap the back buffer with the front buffer
 
 	//Main while loop
 	while (!glfwWindowShouldClose(window)) {
-		//Take care of all GLFW events
-		glfwPollEvents();
+		glClearColor(51.0f / 255.0f, 153.0f / 255.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+		glfwPollEvents(); //Take care of all GLFW events
 	}
 
-	//Delete window
-	glfwDestroyWindow(window);
-	//Terminate GLFW
-	glfwTerminate();
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	glfwDestroyWindow(window);//Delete window
+
+	glfwTerminate();//Terminate GLFW
+
 	return 0;
 }
